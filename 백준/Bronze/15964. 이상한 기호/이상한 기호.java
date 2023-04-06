068c6fe87dceb9864b61bb2eaac855a9026edcb9
red-Pen9uin <53940789+red-Pen9uin@users.noreply.github.com>
2023-04-06 11:29:08 +0900

    [Bronze V] Title: 이상한 기호, Time: 116 ms, Memory: 12912 KB -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        System.out.printf("%d", (A+B)*(A-B));
+
+    }
+}
