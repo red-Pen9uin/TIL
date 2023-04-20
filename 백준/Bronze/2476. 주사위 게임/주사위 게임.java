735688009d2f28e6de3296f45100222ebad66263
red-Pen9uin <53940789+red-Pen9uin@users.noreply.github.com>
2023-04-20 20:20:35 +0900

    [Bronze III] Title: 주사위 게임, Time: 188 ms, Memory: 18324 KB -BaekjoonHub

--- 백준/Bronze/2476. 주사위 게임/주사위 게임.java
+++ 백준/Bronze/2476. 주사위 게임/주사위 게임.java
@@ -1,41 +1,49 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        int sum[] = new int[N];
-
-        for(int i = 0; i < N; i ++) {
-            int x = scanner.nextInt();
-            int y = scanner.nextInt();
-            int z = scanner.nextInt();
-
-            if (x == y && y == z && x == z) {
-                sum[i] = 10000 + (x * 1000);
-            }
-            else if (x == y || x == z) {
-                sum[i] = 1000 + (x * 100);
-            }
-            else if (y==z) {
-                sum[i] = 1000 + (y * 100);
-            }
-            else if (x>y && x>z){
-                sum[i] = x*100;
-            }
-            else if (y>x && y>z){
-                sum[i] = y*100;
-            }
-            else if (z>y && z>x){
-                sum[i] = z*100;
-            }
-        }
-        int max = 0;
-        for(int j = 0 ; j < sum.length ; j++) {
-            if(sum[j] > max) {
-                max = sum[j];
-            }
-        }
-        System.out.printf("%d", max);
-    }
+	static int max_num(int[] array) {
+		int max = 0;
+		for(int i=0; i< array.length; i++) {
+			if(array[i] > max) max = array[i];
+		}
+		return max;
+	}
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int user = scanner.nextInt();
+		int money[] = new int[user];
+		int max = 0;
+		
+		for (int i = 0; i < user; i++) {
+			int a = scanner.nextInt();
+			int b = scanner.nextInt();
+			int c = scanner.nextInt();
+			
+			if (a == b && b == c) {
+				money[i] = 10000 + a * 1000;
+			} else if (a == b || a == c) {
+				money[i] = 1000 + a * 100;
+			} else if (b == c) {
+				money[i] = 1000 + b * 100;
+			} else {
+				if (a > b && a > c) {
+					money[i] = a * 100;
+				} else if (b > a && b > c) {
+					money[i] = b * 100;
+				} else {
+					money[i] = c * 100;
+				}
+			}
+		}
+//		if(money[0] > money[1] && money[0] > money[2]) {
+//			max = money[0];
+//		} else if(money[1] > money[0] && money[1] > money[2]) {
+//			max = money[1];
+//		} else {
+//			max = money[2];
+//		}
+		// System.out.println(max);
+		System.out.println(max_num(money));
+	}
 }
